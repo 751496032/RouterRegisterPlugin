eb78b290e5a5f11f25d8ef72f2b0fc2600e5c8b5
XML <13480465265@126.com>
2024-10-20 23:40:25 +0800

    text.ts

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,6 +1,6 @@
 
 function isEmpty(obj: string | any | undefined | null) {
-    return obj === undefined || obj === null || obj.trim().length === 0
+    return obj === undefined || obj === null || (typeof obj ==='string' && obj.trim() === '')
 }
 
 function isNotEmpty(obj: string | any | undefined | null) {
